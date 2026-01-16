877de2534c91aadc03d049f1d9d8d2d3c3bc4c18
olucaxx <devpereiralucas@gmail.com>
2026-01-16 09:02:06 -0300

    bug fix - added key mapping

--- snake-the-game/snake.py
+++ snake-the-game/snake.py
@@ -1,25 +1,26 @@
 import pygame
 
+# mapeamento dos botoes
+KEY_MAP = {
+    pygame.K_UP:    (-1, 0),
+    pygame.K_DOWN:  (1, 0),
+    pygame.K_LEFT:  (0, -1),
+    pygame.K_RIGHT: (0, 1),
+} 
+
 class Snake:
     def __init__(self):
         self.body_sections = [(24,22),(24,23),(24,24),(24,25),(24,26)]
         self.mov_direction = (0,-1)
 
-    def get_new_direction(self, key):
-        if key == pygame.K_UP:
-            return (-1, 0)
-        if key == pygame.K_DOWN:
-            return (1, 0)
-        if key == pygame.K_LEFT:
-            return (0, -1)
-        if key == pygame.K_RIGHT:
-            return (0, 1)
-        
     def change_direction(self, key) -> bool:
-        new_direction = self.get_new_direction(key)
+        new_direction = KEY_MAP.get(key)
 
-        if new_direction != (-self.mov_direction[0], -self.mov_direction[1]):
-            self.mov_direction = new_direction
+        # garante que tenhamos um movimento valido
+        if new_direction is not None: 
+            # garante que voce nao consiga andar de volta em si mesmo
+            if new_direction != (-self.mov_direction[0], -self.mov_direction[1]):
+                self.mov_direction = new_direction
 
     def move(self):
         self.body_sections.insert(0, 
